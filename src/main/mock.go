8c4ae3ad188ebd969cd022287687e5ac50256fd0
fede118 <fede.cano118@gmail.com>
2019-08-28 20:11:00 -0300

    clean up

--- src/main/mock.go
+++ src/main/mock.go
@@ -19,15 +19,18 @@ func main(){
 
 }
 
+func mock() {
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	time.Sleep(time.Millisecond * 500)
-	fmt.Println(w, "Respuesta recibida")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, "HOMEPAGE")
+	http.HandleFunc("/users/", homePageUsers)
+	http.HandleFunc("/countries/", homePageCountries)
+	http.HandleFunc("/sites/", homePageSites)
+
+	http.HandleFunc("/users/ping", pong)
+	http.HandleFunc("/countries/ping", pong)
+	http.HandleFunc("/sites/ping", pong)
+
+	http.ListenAndServe(port, nil)
 }
-
 
 func homePageUsers(w http.ResponseWriter, r *http.Request) {
 	time.Sleep(time.Millisecond * 500)
@@ -53,19 +56,6 @@ func homePageSites(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, domains.MockMap["sites"])
-}
-
-func mock() { // para hcacer un get necesitamos un browser y para un browser necesitamos un servidor
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/users/", homePageUsers)
-	http.HandleFunc("/countries/", homePageCountries)
-	http.HandleFunc("/sites/", homePageSites)
-
-	http.HandleFunc("/users/ping", pong)
-	http.HandleFunc("/countries/ping", pong)
-	http.HandleFunc("/sites/ping", pong)
-
-	http.ListenAndServe(port, nil)
 }
 
 func pong(w http.ResponseWriter, r *http.Request) {
